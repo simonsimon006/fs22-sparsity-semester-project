8a832febb01f1d0d178316b9a01ca4fae01f4cbb
Simon Grunwald <simongru@student.ethz.ch>
2022-04-26 23:18:24 +0200

    Adds prototype for plotting.

--- src/plotter.py
+++ src/plotter.py
@@ -2,14 +2,14 @@
 
 from pathlib import Path
 from typing import List, Tuple
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
 
+import matplotlib.gridspec as gridspec
+import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
-from numpy import ndarray, absolute, subtract, arange, meshgrid
-from numpy.fft import rfft
-from torch import Tensor
 from numba import jit
+from numpy import absolute, arange, meshgrid, ndarray, subtract
+from numpy.fft import rfft
+from scipy.stats import pearsonr
 
 
 # From the previous project
@@ -55,7 +55,7 @@ def __diff(axs: Axes, original: ndarray, denoised: ndarray):
 	x, t = arange(x), arange(t)
 
 	X, T = meshgrid(x, t)
-	
+
 	axs.set_xlabel("Spatial axis")
 	axs.set_ylabel("Temporal axis")
 	axs.set_zlabel("Strain axis")
@@ -63,27 +63,134 @@ def __diff(axs: Axes, original: ndarray, denoised: ndarray):
 	axs.plot_surface(X, T, diff, color="blue")
 
 
+@jit(parallel=True)
+def __variation(y_pred: ndarray, y_true: ndarray) -> float:
+	'''Computes the variation coefficient.'''
+	sigma = (np.square(y_pred - y_true)).mean()
+	mean = y_pred.mean()
+	if mean == 0:
+		mean = np.NaN
+	return sigma / mean
+
+
+@jit(parallel=True)
+def __var_and_corr(pred: ndarray, true: ndarray) -> Tuple[float, float]:
+	var, (corr, _) = __variation(pred, true), pearsonr(pred, true)
+	return var, corr
+
+
 def __diag(axs: Axes, original: ndarray, denoised: ndarray):
-	pass
+	flat_original, flat_denoised = original, denoised.flatten()
+	axs.set_title("45 deg correlation plot")
+	axs.set_xscale("log")
+	axs.set_yscale("log")
+	axs.scatter(flat_original,
+	            flat_denoised,
+	            label="Signals",
+	            marker=".",
+	            s=0.3)
+	axs.axsline(xy1=(0, 0),
+	            xy2=(1, 1),
+	            linestyle="--",
+	            color="yellow",
+	            transform=axs.transAxes)
+	axs.set_ylabel("Original")
+	axs.set_xlabel("Denoised")
+	axs.set_title("Signal comparision")
+
+	axs.set_aspect("equal", 'box')
+
+	var, corr = __var_and_corr(denoised, original)
+	axs.text(
+	    0.7,
+	    0.9,
+	    f"Var: {var:.3f}\nCorr: {corr:.3f}",
+	    bbox={
+	        'alpha': 0.2,
+	        'pad': 10,
+	        "boxstyle": "Round,pad=0.01",
+	        "facecolor": "blue"
+	    },
+	    transform=axs.transAxes,
+	)
+
+	xll, xul = axs.get_xlim()
+	yll, yul = axs.get_ylim()
+	ll = min(xll, yll)
+	up = max(xul, yul)
+	axs.set_xlim(left=ll, right=up)
+	axs.set_ylim(bottom=ll, top=up)
+	axs.legend()
 
 
 def __time_and_space(time_axs: Axes, space_axs: Axes, original: ndarray,
                      denoised: ndarray):
-	pass
+	'''Simply plots the signals and their difference in a log scale onto the argument axes.'''
 
+	# They should have the same shape
+	assert (original.shape == denoised.shape)
 
-def plot(original: ndarray | Tensor, denoised: ndarray | Tensor,
-         result: ndarray | Tensor, save: str | Path) -> None:
-	"""Produes the relevant plots from the data."""
-	'''# First we transform to ndarrays
-	match data:
-		case ndarray(data):
-			pass
-		case Tensor(data):
-			data = data.numpy()
+	tmax, xmax = original.shape
+	# Display at time t
+	# 0.65mm stepsize
+	t = tmax // 4  # Talked with Yasmin about that. She says the time is right.
+	xax = arange(0, 0.65 * len(original[t, :]), 0.65)
+	time_axs.set_title(f"Time fixed at timestep {t}.")
+	time_axs.plot(xax, denoised[t, :], label="Denoised", color="cyan")
+	time_axs.plot(xax,
+	              original[t, :],
+	              label="Original",
+	              linewidth=MSIZE,
+	              alpha=0.2,
+	              color="black")
 	'''
+	time_axs.plot(
+		xax,
+		filtered,
+		label="Hand denoised",
+		#linewidth=msize,
+		ls="dotted",
+		color="red")
+	'''
+	time_axs.set_ylabel("Strain")
+	time_axs.set_xlabel("Location/mm")
+	time_axs.legend()
+
+	# Display at pos x
+	# unknown time steps
+	x = xmax // 4  # Talked with Yasmin about that. She says the time is right.
+	xax = arange(0, len(original[:, x]))
+	space_axs.set_title(f"Location fixed at pos {x * 0.65}mm.")
+	space_axs.plot(xax, denoised[:, x], label="Denoised", color="cyan")
+	space_axs.plot(xax,
+	               original[:, x],
+	               label="Original",
+	               linewidth=MSIZE,
+	               alpha=0.2,
+	               color="black")
+	'''space_axs.plot(
+		xax,
+		filtered,
+		label="Hand denoised",
+		#linewidth=msize,
+		ls="dotted",
+		color="red")
+	'''
+	space_axs.set_ylabel("Strain")
+	space_axs.set_xlabel("Timesteps")
+	space_axs.legend()
+
+
+def plot(original: ndarray, denoised: ndarray, result: ndarray,
+         save: str | Path) -> None:
+	"""Produes the relevant plots from the data."""
+
 	fig = plt.figure(figsize=(12, 10), dpi=300)
 	axs = __make_axes(fig)
 
+	__time_and_space(axs[0], axs[1], original, denoised)
+	__coeffs(axs[2], result)
+	__diag(axs[3], original, denoised)
+	__diff(axs[4], original, denoised)
 	fig.savefig(save)
 	plt.close(fig)
