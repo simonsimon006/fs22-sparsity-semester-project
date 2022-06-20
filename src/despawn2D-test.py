4003af88176bdc49e2932337d67c7a225c588575
Simon Grunwald <simongru@student.ethz.ch>
2022-06-20 19:04:33 +0200

    Massive cleanup + RLP

--- src/despawn2D-test.py
+++ src/despawn2D-test.py
@@ -1,12 +1,16 @@
 from re import S
-from despawn2D import Despawn2D
 
-from torch.optim import Adadelta as Opt
-from torch.nn import MSELoss as MainLoss, L1Loss
-from torch import tensor, norm, save, sin, linspace, pi, manual_seed, zeros_like, device, rand_like, set_default_dtype, float64
 import matplotlib.pyplot as plt
 from pywt import Wavelet
+from torch import (device, float64, linspace, manual_seed, norm, pi, rand_like,
+                   save, set_default_dtype, sin, tensor, zeros_like)
+from torch.nn import L1Loss
+from torch.nn import MSELoss as MainLoss
 from torch.nn.utils.clip_grad import clip_grad_norm_
+from torch.optim import Adadelta as Opt
+from torch.optim.lr_scheduler import ReduceLROnPlateau as RLP
+
+from despawn2D import Despawn2D
 
 # Set random seed for desterministic behaviour
 manual_seed(0)
@@ -16,17 +20,17 @@ device = device("cpu")
 TESTING_FREQ = 5 * 2 * pi  # Hz * rad
 AMPLITUDE = 10
 ADAPT_FILTERS = False
-EPOCHS = 55
+EPOCHS = 1000
 LEARNING_RATE = 2 * 1e1
 
 # Random choice
-WAVELET_NAME = "bior1.1"
+WAVELET_NAME = "db8"
 
 SCALING = tensor(Wavelet(WAVELET_NAME).dec_hi, device=device)
 print(SCALING.shape)
 REG_FACT = 0
 
-time = linspace(0, 5, 3000, device=device)
+time = linspace(0, 5, 120, device=device)
 signal = AMPLITUDE * sin(TESTING_FREQ * time).repeat(250, 1)
 noise = rand_like(signal, device=device)
 #noise = zeros_like(signal, device=device)
@@ -34,15 +38,16 @@ data = (signal + noise)
 
 loss_fn = MainLoss(reduction="sum")
 #regularization = L1Loss(reduction="sum")
-model = Despawn2D(12, SCALING, adapt_filters=ADAPT_FILTERS)
+model = Despawn2D(2, SCALING, adapt_filters=ADAPT_FILTERS)
 model = model.to(device)
 
 optimizer = Opt(model.parameters(), lr=LEARNING_RATE)
+scheduler = RLP(optimizer, 'min', patience=2, factor=0.5)
 
 for epoch in range(EPOCHS):
 	optimizer.zero_grad()
 
-	denoised, wav_coeffs = model(signal)
+	denoised, wav_coeffs = model(data)
 
 	# Do regularization
 	reg_loss = norm(wav_coeffs, p=1)
@@ -50,27 +55,32 @@ for epoch in range(EPOCHS):
 
 	loss = error + REG_FACT * reg_loss
 	#clip_grad_norm_(model.parameters(), 1)
-	#loss.backward()
+	loss.backward()
 
-	#optimizer.step()
+	scheduler.step(loss)
 
 	if epoch % 2 == 0:
 		print(
 		    f"Epoch {epoch+1} of {EPOCHS}: Reconstruction error: {float(error)}; Regularization penalty: {float(reg_loss)}; total loss: {float(loss)}"
 		)
-print(list(model.parameters()))
+
 plt.plot(time.cpu(), denoised.cpu().detach().numpy()[0, :], label="Denoised")
 
 plt.plot(time.cpu(),
          signal.cpu().detach().numpy()[0, :],
          label="Signal",
          alpha=0.2)
+plt.plot(time.cpu(),
+         data.cpu().detach().numpy()[0, :],
+         label="Noisy signal",
+         alpha=0.5)
 plt.legend()
-plt.show()
-exit()
+
 plt.savefig(
     f"sine-figures/despawn2d-{EPOCHS}-sine-{TESTING_FREQ}_Hz-amp-{AMPLITUDE}-adapt_filters-{ADAPT_FILTERS}-wavelet-{WAVELET_NAME}.svg"
 )
+
+plt.show()
 save(
     model,
     f"models/despawn2d-{EPOCHS}-sine-{TESTING_FREQ}_Hz-amp-{AMPLITUDE}-wavelet-{WAVELET_NAME}.pt"
--- src/despawn2D.py
+++ src/despawn2D.py
@@ -11,33 +11,7 @@ from torch.fft import irfft, rfft
 
 from torch.nn.functional import conv1d, pad, sigmoid, conv_transpose1d
 from torch.nn.parameter import Parameter
-from torch.nn import ReflectionPad1d, ReplicationPad1d
-
-NORM = "backward"
-PAD_MODE = "circular"
-
-
-def even_odd(input):
-	'''Splits a sequence into even and odd components.'''
-	# So we can split the wavelet stuff and the inputs signals with
-	# the same function.
-	if len(input.shape) == 1:
-		return input[::2], input[1::2]
-	else:
-		return input[:, ::2], input[:, 1::2]
-
-
-def rfft_two(input1, input2, common_desired_length, normalization="ortho"):
-	'''Rffts both inputs zero-padded to the common_desired_length.'''
-	return rfft(input1, common_desired_length,
-	            norm=normalization), rfft(input2,
-	                                      common_desired_length,
-	                                      norm=normalization)
-
-
-def prepare(coeffs, desired_half_length, norm="ortho"):
-	'''Prepares the given input for lifting by first splitting them into even and odd parts and then rfft'ing them zero-padded to the desired (half) length.'''
-	return rfft_two(*even_odd(coeffs), desired_half_length, normalization=norm)
+from torch.nn import ReplicationPad1d
 
 
 def sane_circular_pad(input: Tensor, to_pad):
@@ -63,20 +37,37 @@ def sane_circular_pad(input: Tensor, to_pad):
 	return repeated[:, :(in_len + to_pad)]
 
 
-def convolve(input, filter, phase=1, tr=False):
-	'''This function is my attempt at using the FFT to do the convolution. It is much faster than the other convolution function further down. But a time shift occurs if using this one. I vaguely know why this might be, padding and stuff, but I am not (yet) willing to figure it out in detail and fix it.'''
+def compute_wavelet(scaling: Tensor) -> Tensor:
+	'''Computes the wavelet filter coefficients from the scaling coefficients. The formula is g_k = h_(M-k-1) where M is the filter length of h. The formula stems from a paper.'''
+
+	# flip copies / clones the input.
+	g = flip(scaling, (-1, ))
+	g[1::2] *= -1
+
+	return g
+
+
+# Only required for biorthogonal instead of orthogonal wavelets.
+def compute_scaling_synthesis(scaling: Tensor) -> Tensor:
+	return flip(scaling, (-1, ))
+
+
+def compute_wavelet_synthesis(scaling: Tensor) -> Tensor:
+	# flip copies / clones the input.
+	g = flip(scaling, (-1, ))
+	g[::2] *= -1
+
+	return g
+
+
+def convolve(input, filter, transpose=False):
+	'''This function is my attempt at using the FFT to do the convolution. It is much faster than the normal convolution function from PyTorch.'''
 
 	# Length is the length of the input. Some problems with that.
 	input_length = input.shape[-1]
-	filter_length = filter.shape[-1]
-	'''if input_length < filter_length:
-		input = sane_circular_pad(input, filter_length - input_length)
 
-		ra = rfft(input)
-		rb = rfft(filter)
-	else:'''
 	ra = rfft(input)
-	if tr:
+	if transpose:
 		rb = conj(rfft(filter, n=input_length))
 	else:
 		rb = rfft(filter, n=input_length)
@@ -86,91 +77,21 @@ def convolve(input, filter, phase=1, tr=False):
 	return res
 
 
-def fconvolve(input, filter, tr=False, **kwargs):
-	'''Basic convolution with (not yet) circular padding.'''
-
-	original_len = input.shape[-1]
-
-	# Padding the input so the convolution returns as many coefficients
-	# as there were entries in the input before. The "same" padding of the conv
-	# does not work nicely.
-
-	input = sane_circular_pad(input, filter.shape[-1])
-
-	input = unsqueeze(input, dim=1)
-	filter = unsqueeze(unsqueeze(filter, dim=0), dim=0)
-
-	if tr:
-		res = squeeze(conv_transpose1d(input, filter), 1)
-	else:
-		res = squeeze(conv1d(input, filter), 1)
-
-	return res[:, :original_len]
-
-
 class ForwardTransformLayer(nn.Module):
 	'''Expects a tensor with shape (n, m) and computes one forward step over each row, i.e. it iterates over all values of 0...n and then computes the lifting. input_length needs to be equal to m.'''
 
 	def __init__(self, scaling):
 		super(ForwardTransformLayer, self).__init__()
 
-		# Set normalization mode for rfft and irfft.
-		self.norm = NORM
-
 		# Store the scaling function filter.
 		self.scaling = scaling
 
-		# Applies an approx. 45 deg phase shift to compensate for that in the
-		# output.
-		self.phase_correction = exp(tensor(1j * 0.785398))
-
-	def compute_wavelet(self, scaling: Tensor) -> Tensor:
-		'''Computes the wavelet filter coefficients from the scaling coefficients. The formula is g_k = h_(M-k-1) where M is the filter length of h. The formula stems from a paper.'''
-
-		# flip copies / clones the input.
-		g = flip(scaling, (-1, ))
-		g[1::2] *= -1
-
-		return g
-
 	def forward(self, input):
 
-		wavelet = self.compute_wavelet(self.scaling)
-		'''
-		# It is expected that the inputs rows have the length equal to a power
-		# of two.
-		part_length = input.shape[1] // 2
+		wavelet = compute_wavelet(self.scaling)
 
-		# Splits the filter coefficients into even and odd parts and
-		# transforms the wavelet and scaling coefficients.
-		# Zero-padding happens in prepare_coefficients.
-		wavelet_even, wavelet_odd = even_odd(wavelet)
-		scaling_even, scaling_odd = even_odd(self.scaling)
-
-		# Split the input into even and odd.
-		even, odd = even_odd(input)
-
-		# Create the detail coefficients.
-		details = convolve(
-		    even, wavelet_even, norm=self.norm, length=part_length) + convolve(
-		        odd, wavelet_odd, norm=self.norm, length=part_length)
-
-		# Filter out the approximation coefficients.
-		approximation = convolve(
-		    even, scaling_even, norm=self.norm, length=part_length) + convolve(
-		        odd, scaling_odd, norm=self.norm, length=part_length)
-
-		if len(details.shape) == 1:
-			details = unsqueeze(details, dim=-1)
-			approximation = unsqueeze(approximation, dim=-1)
-
-		return details, approximation
-		'''
-
-		details = convolve(input, wavelet, phase=self.phase_correction)
-		approximation = convolve(input,
-		                         self.scaling,
-		                         phase=self.phase_correction)
+		details = convolve(input, wavelet)
+		approximation = convolve(input, self.scaling)
 
 		return details[:, ::2], approximation[:, ::2]
 
@@ -181,37 +102,13 @@ class BackwardTransformLayer(nn.Module):
 	def __init__(self, scaling):
 		super(BackwardTransformLayer, self).__init__()
 
-		# Set normalization mode for rfft and irfft.
-		self.norm = NORM
-
 		# Store the filters.
 		self.scaling = scaling
-
-	def compute_wavelet(self, scaling: Tensor) -> Tensor:
-		'''Computes the wavelet filter coefficients from the scaling coefficients. The formula is g_k = h_(M-k-1) where M is the filter length of h. The formula stems from a paper.'''
-
-		# flip copies / clones the input.
-		g = flip(scaling, (-1, ))
-		g[1::2] *= -1
-
-		return g
-
-	# These are required for biorthogonal wavelets
-	def compute_scaling_synthesis(self, scaling: Tensor) -> Tensor:
-		return flip(scaling, (-1, ))
-
-	def compute_wavelet_synthesis(self, scaling: Tensor) -> Tensor:
-		# We have to clone it so we do not change the wavelet tensor for
-		# the rest of the class.
-		g_hat = clone(scaling)
-		g_hat[::2] *= -1
-
-		return g_hat
 
 	def forward(self, input):
 
 		# Compute synthesis filter coefficients
-		wavelet_synthesis = self.compute_wavelet(self.scaling)
+		wavelet_synthesis = compute_wavelet(self.scaling)
 		scaling_synthesis = self.scaling
 
 		# Pad the input to an even size.
@@ -229,13 +126,13 @@ class BackwardTransformLayer(nn.Module):
 		filled_details[:, ::2] = details
 		filled_approximation[:, ::2] = approximation
 
-		# Get the lengths of the signals.
-		fil_details_len = filled_details.shape[1]
-		fil_approx_len = filled_approximation.shape[1]
-
 		# Convolve with the reconstruction filters.
-		details_rec = convolve(filled_details, wavelet_synthesis, tr=True)
-		approx_rec = convolve(filled_approximation, scaling_synthesis, tr=True)
+		details_rec = convolve(filled_details,
+		                       wavelet_synthesis,
+		                       transpose=True)
+		approx_rec = convolve(filled_approximation,
+		                      scaling_synthesis,
+		                      transpose=True)
 
 		return details_rec + approx_rec
 
@@ -248,20 +145,9 @@ class HardThreshold(nn.Module):
 		self.b_plus = Parameter(b_plus)
 		self.b_minus = Parameter(b_minus)
 
-	def denominator(self, x, alpha, bias):
-		res = 1. + exp(alpha * (x + bias))
-		return res
-
 	def forward(self, input):
 		return input * (sigmoid(self.alpha * (input - self.b_plus)) +
 		                sigmoid(-self.alpha * (input + self.b_minus)))
-
-	'''
-	def forward(self, input):
-		return multiply(
-		    input, ((1. / self.denominator(input, self.alpha, self.b_minus)) +
-		            (1. / self.denominator(input, -self.alpha, -self.b_plus))))
-	'''
 
 
 class Despawn2D(nn.Module):
@@ -284,7 +170,7 @@ class Despawn2D(nn.Module):
 		]
 
 		# Define threshold parameters similar to the paper
-		self.alpha = tensor(5.)
+		self.alpha = tensor(10.)
 		self.b_plus = tensor(2.)
 		self.b_minus = tensor(1.)
 
@@ -306,10 +192,6 @@ class Despawn2D(nn.Module):
 		# Pads the input if the input size is not a power of two.
 		# The circular padding is necessary for perfect reconstruction.
 		# And it gives the least amount of coefficients.
-		'''input = sane_circular_pad(
-		    input,
-		    desired_input_length - input_length,
-		)'''
 		to_pad = ((desired_input_length - input_length) // 2,
 		          ceil((desired_input_length - input_length) / 2))
 		padder = ReplicationPad1d(to_pad)
@@ -323,14 +205,16 @@ class Despawn2D(nn.Module):
 		wav_coeffs.append(approximation)
 
 		# Filter the coefficients.
-		#filtered = list(map(self.threshold, wav_coeffs))
-		filtered = list(wav_coeffs)
+		filtered = list(map(self.threshold, wav_coeffs))
+		#filtered = list(wav_coeffs)
+
 		# Transform the filtered inputs back.
 		approximation = filtered.pop()
 		for transform in self.backward_transforms:
 			details = filtered.pop()
 			approximation = transform((details, approximation))
-
+		# The index stuff with approximation removes the padding. The reduc
+		# call concatenates all wavelet coefficients into one big tensor.
 		return approximation[:, to_pad[0]:input_length + to_pad[1]], reduce(
 		    lambda tensor1, tensor2: concat(tensors=(tensor1, tensor2), dim=-1),
 		    wav_coeffs)
